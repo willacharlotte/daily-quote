71612d6ed97dc683a59906530bcb5a4c269b53ae
Willa Lyle <Willa.Lyle@sanlam.co.za>
2023-08-17 17:35:41 +0200

    feat: update QuoteRepository to pull from tempQuotes.ts'

--- src/backend/repositories/QuoteRepository.ts
+++ src/backend/repositories/QuoteRepository.ts
@@ -1,19 +1,53 @@
 import { Quote } from "../schemas/Quote";
+import { QUOTES } from "./tempQuotes";
 
 export namespace QuoteRepository {
-  const getByDate = (date: Date): Quote | null => {
-    return null;
+  const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const parseDate = (ddMMMDate: string): Date => {
+    const [day, month] = ddMMMDate.split(" ");
+    const monthIndex = MONTHS.findIndex((m) => m === month);
+    return new Date(2000, monthIndex >= 0 ? monthIndex : 0, parseInt(day));
   };
 
-  const getByAuthor = (author: string): Quote | null => {
-    return null;
+  export const getByDate = (date: Date): Quote | undefined => {
+    return getAll().find(
+      (quote) =>
+        quote.date.getDate() === date.getDate() &&
+        quote.date.getMonth() === date.getMonth()
+    );
   };
 
-  const getRandom = (): Quote | null => {
-    return null;
+  export const getByAuthor = (author: string): Quote | undefined => {
+    return getAll().find((quote) => quote.author === author);
   };
 
-  const getAll = (): Quote[] => {
-    return [];
+  export const getRandom = (): Quote | undefined => {
+    const quotes = getAll();
+    return quotes.length === 0
+      ? undefined
+      : quotes[Math.floor(Math.random() * quotes.length)];
+  };
+
+  export const getAll = (): Quote[] => {
+    return QUOTES.map((quote) => {
+      return {
+        ...quote,
+        date: parseDate(quote.date),
+      };
+    });
   };
 }

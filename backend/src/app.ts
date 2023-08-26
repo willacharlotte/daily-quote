5601d2c536c4711cd8b50b446ac7fb6b015fd8e6
Lisa Nolte <lisan@bbd.co.za>
2023-08-26 12:56:47 +0200

    Changing to live url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,24 +3,33 @@ import { ENV } from "./config/env";
 import apiMiddleware from "./middleware";
 import quotesRoutes from "./routes/quotes";
 import cors from 'cors';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
+
+const options = {
+  key: fs.readFileSync('/path/to/private-key.pem'), // Path to your private key file
+  cert: fs.readFileSync('/path/to/certificate.pem'), // Path to your certificate file
+};
 
 app.use(apiMiddleware);
 
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: 'https://georgep.link:443',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 }));
 
 app.use(quotesRoutes);
 
-app.listen(ENV.PORT, () => {
+const httpsServer = https.createServer(options, app);
+
+httpsServer.listen(ENV.PORT, () => {
   console.log(`App listening on ${ENV.PORT}`);
   if (ENV.ENVIRONMENT === "dev") {
     console.log(
-      `psst, here's a link, you lazy potato: http://localhost:${ENV.PORT}/`
+      `psst, here's a link, you lazy potato: https://georgep.link:444`
     );
   }
 });

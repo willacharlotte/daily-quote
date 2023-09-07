5fb30cb7f087188f2e296a767c80ce999ff2d8eb
Lisa Nolte <lisan@bbd.co.za>
2023-09-07 20:25:10 +0200

    trying something

--- backend/src/controllers/quotes/getAll.ts
+++ backend/src/controllers/quotes/getAll.ts
@@ -2,24 +2,33 @@ import { Request, Response } from "express";
 import { QuoteRepository } from "../../repositories/QuoteRepository";
 
 export const getAll = async (req: Request, res: Response) => {
-  const { data: quotes, messages } = await QuoteRepository.getAll();
-
-  if (!quotes) {
-    console.error("An error occurred:", ...messages);
-    res.status(500);
-    res.json({
-      error: "An error occurred while fetching the quotes.",
-      messages,
+  try{
+    const { data: quotes, messages } = await QuoteRepository.getAll();
+  
+    if (!quotes) {
+      console.error("An error occurred:", ...messages);
+      res.status(500);
+      res.json({
+        error: "An error occurred while fetching the quotes.",
+        messages,
+      });
+      return;
+    }
+  
+    if (quotes.length === 0) {
+      res.status(404);
+      res.json({ messages });
+      return;
+    } else {
+      res.json(quotes);
+      return;
+    }
+  } catch (err){
+      var error = err as Error;
+      res.status(500);
+      res.json({
+      error: "An error occurred while processing the request.",
+      message: error.message,
     });
-    return;
-  }
-
-  if (quotes.length === 0) {
-    res.status(404);
-    res.json({ messages });
-    return;
-  } else {
-    res.json(quotes);
-    return;
   }
 };
